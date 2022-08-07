9888f75327609f52fc8bce2eca0a1dd8dcee89a7
Jimvid <victor@jimvid>
2022-08-07 12:11:33 +0200

    ✨ Initial commit

---
+++ setup.sh
@@ -0,0 +1,39 @@
+# Install xcode build tools
+xcode-select --install
+
+# Install & update homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew update
+
+# Install brew casks
+xargs brew install < brew-casks.txt
+
+# Install brew formulae
+xargs brew install < brew-formulae.txt
+
+# setup dotfiles
+cd ~ 
+
+curl -O https://raw.githubusercontent.com/jimvid/dotfiles/master/.bash_profile
+
+# Condigure github user
+git config --global user.name jimvid
+git config --global user.email victor@jimvid.com
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Install latest and LTS node versions
+nvm install lts
+nvm install latest
+
+# Use latest version
+nvm use latest
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+npm install -g eslint
+
+# Create a dev folder
+mkdir ~/dev
